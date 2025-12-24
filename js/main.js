cf392407bba3c2f2b975bb8daee8d5ce67ec08ae
Matias-coc <matiasing0422@gmail.com>
2025-12-24 11:01:26 -0300

    se arreglo el while y agregue saludo final

--- js/main.js
+++ js/main.js
@@ -14,8 +14,39 @@ const datoPrestamo = () => {
     let cuotas = parseInt(prompt("ingrese cantidad de cuotas:"));
 
     return {monto, cuotas}; 
+};
+
+const calculo = (monto, cuotas, interesPrestamo) => {
+    const interes = monto * interesPrestamo;
+    const total = monto + interes;
+    const cuotaMensual = total / cuotas;
+
+    return {interes, total, cuotaMensual};
+};
+
+// Resultado
+
+const resultado = (monto, cuotas, total, cuotaMensual) => {
+    let mensaje = "-RESUMEEN-\n" + "Monto: $" + monto + "\n" + "Cuotas:" + cuotas + "\n" + "Total a pagar: $" + total + "\n" + "Valor de la cuota: $" + cuotaMensual;
+
+    console.log(mensaje);
+    alert(mensaje);
+};
+
+// While
+
+while (continuar) {
+    let datos = datoPrestamo();
+    
+    let resultados = calculo(datos.monto, datos.cuotas, interesPrestamo);
+
+    resultado(datos.monto, datos.cuotas, resultados.total, resultados.cuotaMensual)
+
+    let  respuesta = confirm("¿Desea simular otro prestamo?");
+    if (respuesta == false){
+        continuar = false; 
+    }
+
 }
 
-const calculo = () => {
-
-}
+console.log("Gracias por usar el simulador")
